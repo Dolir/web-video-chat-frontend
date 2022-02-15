ad19fc7de8f1790d3bfeb3a763a030559fa2a904
Dolir <dinmuhammed@doovertech.com>
2022-02-15 19:11:00 +0600

    init

---
+++ components/home/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styles from '../../styles/Home.module.scss'
+const Navbar = () => {
+  return <div className={styles.main}>
+    <div>some logo</div>
+    <div>navigations</div>
+    <div>Log in</div>
+  </div>
+}
+export default Navbar
