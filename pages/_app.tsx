f579c068b57b3258735ac711b1df61a922534e8a
Dolir <dinmuhammed@doovertech.com>
2022-02-17 13:21:35 +0600

    fixing animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,29 @@ import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { motion, AnimatePresence } from "framer-motion"
+ import Router from "next/router";
+
 function MyApp({ Component, pageProps, router }: AppProps) {
+      // Add that code to _app.tsx / _app.jsx
+
+     
+      const routeChange = () => {
+        // Temporary fix to avoid flash of unstyled content
+        // during route transitions. Keep an eye on this
+        // issue and remove this code when resolved:
+        // https://github.com/vercel/next.js/issues/17464
+  
+        const tempFix = () => {
+          const allStyleElems = document.querySelectorAll('style[media="x"]');
+          allStyleElems.forEach((elem) => {
+            elem.removeAttribute("media");
+          });
+        };
+        tempFix();
+      };
+  
+     Router.events.on("routeChangeComplete", routeChange );
+     Router.events.on("routeChangeStart", routeChange );
   return (
     <AnimatePresence>
       <motion.div
@@ -21,7 +43,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
             opacity: 0,
           }
         }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 2 }}
       >
         <Component {...pageProps} />
       </motion.div>
