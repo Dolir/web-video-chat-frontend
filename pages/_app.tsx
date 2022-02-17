a86be3bd6dd764023bdda4cdcb4f60abeff92bba
Dolir <dinmuhammed@doovertech.com>
2022-02-17 13:27:15 +0600

    Fixing animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,29 +2,31 @@ import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { motion, AnimatePresence } from "framer-motion"
- import Router from "next/router";
-
+import Router from "next/router"
+import { useEffect } from "react"
 function MyApp({ Component, pageProps, router }: AppProps) {
-      // Add that code to _app.tsx / _app.jsx
+  // Add that code to _app.tsx / _app.jsx
 
-     
-      const routeChange = () => {
-        // Temporary fix to avoid flash of unstyled content
-        // during route transitions. Keep an eye on this
-        // issue and remove this code when resolved:
-        // https://github.com/vercel/next.js/issues/17464
-  
-        const tempFix = () => {
-          const allStyleElems = document.querySelectorAll('style[media="x"]');
-          allStyleElems.forEach((elem) => {
-            elem.removeAttribute("media");
-          });
-        };
-        tempFix();
-      };
-  
-     Router.events.on("routeChangeComplete", routeChange );
-     Router.events.on("routeChangeStart", routeChange );
+  const routeChange = () => {
+    // Temporary fix to avoid flash of unstyled content
+    // during route transitions. Keep an eye on this
+    // issue and remove this code when resolved:
+    // https://github.com/vercel/next.js/issues/17464
+
+    const tempFix = () => {
+      const allStyleElems = document.querySelectorAll('style[media="x"]')
+      allStyleElems.forEach((elem) => {
+        elem.removeAttribute("media")
+      })
+    }
+    tempFix()
+  }
+
+  Router.events.on("routeChangeComplete", routeChange)
+  Router.events.on("routeChangeStart", routeChange)
+  useEffect(() => {
+    router.push(window.location.pathname)
+  }, [])
   return (
     <AnimatePresence>
       <motion.div
@@ -34,13 +36,13 @@ function MyApp({ Component, pageProps, router }: AppProps) {
         exit="pageExit"
         variants={{
           pageInitial: {
-            opacity: 0,
+            opacity: 0
           },
           pageAnimate: {
-            opacity: 1,
+            opacity: 1
           },
           pageExit: {
-            opacity: 0,
+            opacity: 0
           }
         }}
         transition={{ duration: 0.5 }}
